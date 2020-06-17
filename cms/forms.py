caff0f4c01aeda31946295bc8b14aadcae25e8a7
nagatadaiki <nagatadaiki@nagatadaikinoMacBook-Pro.local>
2020-06-17 12:04:12 +0900

    form

--- cms/forms.py
+++ cms/forms.py
@@ -2,6 +2,9 @@ from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
+from .models import Work
+
+
 UserModel = get_user_model()
 
 
@@ -30,3 +33,18 @@ class UserUpdateForm(forms.ModelForm):
         super().__init__(*args, **kwargs)
         for field in self.fields.values():
             field.widget.attrs['class'] = 'input'
+
+#作品投稿フォーム
+class WorkPostForm(forms.ModelForm):
+    class Meta:
+        model = Work
+        fields = ("name", "contents", "create_user", "create_date", "fav")
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'input'
+
+
+#検索フォーム(まだview,model書いてない)
+#class SearchForm(Forms.form):
+#    word = forms.CharField(max_length=250)
